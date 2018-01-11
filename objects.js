490fb7ad4379dceef7c709a94ed0256469610cd2
Helen <nikishina.olena@gmail.com>
2018-01-11 02:53:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {
  artistName: "before all"
 };
 
-function updatePlaylist(playlist, artistName, songTitle)
-return Object.assign({artistName:`songTitle`} ){
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({artistName:`songTitle`} ){
+}
 }
