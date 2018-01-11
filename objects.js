71fa92a9126c0aba8e78db0ff9308eb39cd9167e
Helen <nikishina.olena@gmail.com>
2018-01-11 03:21:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist,artistName,songTitle){
-  return Object.assign(playlist,{Phil Ochs:['before all']});
+  return Object.assign(playlist,{artistName:['before all']});
 }
 
 function removeFromPlaylist(playlist,artistName){
