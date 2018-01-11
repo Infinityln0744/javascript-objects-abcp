53820f462426a2c4eafd80dc2f38094ec2e77663
Helen <nikishina.olena@gmail.com>
2018-01-11 03:23:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,8 @@ var playlist = {
 };
 
 function updatePlaylist(playlist,artistName,songTitle){
-  return Object.assign(playlist,{artistName:['before all']});
-}
+  playlist[artistName]=songTitle
+  return playlist
 
 function removeFromPlaylist(playlist,artistName){
   delete playlist.artistName;
