52a6fd4ae411571c742ab5a5ff34c37af40728b8
Helen <nikishina.olena@gmail.com>
2018-01-11 03:15:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {
 };
 
 function updatePlaylist(playlist,artistName,songTitle){
-  return Object.assign(playlist, {artistName:['Slowdow','My Bloody Valentine','Phil Ochs']});
+  return Object.assign(playlist, {artistNam:['Slowdow','My Bloody Valentine','Phil Ochs']});
 }
