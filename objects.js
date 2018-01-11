58e14861fa2f3679886f61bb33ad3789f6eb1819
Helen <nikishina.olena@gmail.com>
2018-01-11 03:05:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,7 @@ var playlist = {
  artistName: "before all"
 };
 
-function updatePlaylist(playlist, artistName, songTitle){
-  delete playlist.
+function updatePlaylist(playlist,artistName,songTitle){
+  return Object.assign({}, artistName, {[key]:playlist})
+  const playlist={My Bloody Valentine: Slowdown}
+}
